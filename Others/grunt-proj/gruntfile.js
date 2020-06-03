8251d0850fc09925dd845f02e42bb5cfcdaebfdb
FEhao <hd846@nyu.edu>
2020-06-04 01:07:48 +0800

    .

---
+++ Others/grunt-proj/gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = grunt => {
+
+  //  yarn grunt task_name(like foo, or ignore it to run default)
+
+  // grunt.registerTask('foo', () => {
+  //   console.log(123)
+  // })
+  
+  // grunt.registerTask('bar', '任务描述', () => {
+  //   console.log(123)
+  // })
+
+  // // grunt.registerTask('default', () => {
+  // //   console.log(123)
+  // // })
+
+
+  // grunt.registerTask('async-task', function() {
+  //   const done = this.async()
+  //   setTimeout(() => {
+  //     console.log('async works')
+  //     done(false)
+  //   }, 2000);
+  // })
+
+  // grunt.registerTask('foo', () => {
+  //   console.log(123)
+  //   return false
+  // })
+  
+  // grunt.registerTask('bar', '任务描述', () => {
+  //   console.log(123)
+  // })
+
+  // grunt.registerTask('default', ['foo', 'bar', 'async-task'])
+
+  grunt.initConfig({
+    clean: {
+      temp: 'test/**'
+    }
+  })
+  grunt.loadNpmTasks('grunt-contrib-clean')
+}
+
